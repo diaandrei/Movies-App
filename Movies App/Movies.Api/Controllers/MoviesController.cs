c5a6cae23aa0eb4bd8cd7e59c4bad12135df009a
diaandrei <diamandescuandrei@gmail.com>
2023-11-22 19:55:47 +0000

    Add new API endpoints and custom mapping for enhanced functionality. Implement methods for GET all and GET by id to improve data retrieval.

--- Movies App/Movies.Api/Controllers/MoviesController.cs
+++ Movies App/Movies.Api/Controllers/MoviesController.cs
@@ -7,7 +7,6 @@ using Movies.Contracts.Requests;
 namespace Movies.Api.Controllers
 {
     [ApiController]
-    [Route("api")]
     public class MoviesController : ControllerBase
     {
         private readonly IMovieRepository _movieRepository;
@@ -17,14 +16,36 @@ namespace Movies.Api.Controllers
             _movieRepository = movieRepository;
         }
 
-        [HttpPost("movies")]
+        [HttpPost(ApiEndpoints.Movies.Create)]
         public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
         {
             var movie = request.MapToMovie();
 
             await _movieRepository.CreateAsync(movie);
 
-            return Created($"/api/movies/{movie.Id}", movie);
+            return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.Get)]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+
+            var moviesResponse = movies.MapToResponse();
+            return Ok(moviesResponse);
         }
 
 
--- Movies App/Movies.Api/obj/Debug/net7.0/Movies.Api.AssemblyInfo.cs
+++ Movies App/Movies.Api/obj/Debug/net7.0/Movies.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276fd07380cd82ce82fe4cc962df8a49d375eca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
