1dc52354dcd5f323a9d6f5e5e7880fabf87e6aef
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-06-19 20:55:51 +0100

    Adding basic health check to the api and database

---
+++ Movies App/Movies.Api/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Movies.Application.Database;
+
+namespace Movies.Api.Health
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+        private readonly ILogger<DatabaseHealthCheck> _logger;
+
+        public DatabaseHealthCheck(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            try
+            {
+                _ = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception e)
+            {
+                const string errorMessage = "Failed to establish a connection to the database";
+                _logger.LogError(errorMessage, e);
+                return HealthCheckResult.Unhealthy(errorMessage, e);
+            }
+        }
+    }
+}
