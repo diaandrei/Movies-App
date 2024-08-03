9ef779f055dca64759f66fb9cb5f254c02e8ab18
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-08-03 18:13:16 +0100

    Refactored the rating contreoller and rewrote the identiy extensions

--- Movies App/Movies.Api/Auth/IdentityExtensions.cs
+++ Movies App/Movies.Api/Auth/IdentityExtensions.cs
@@ -2,7 +2,7 @@
 {
     public static class IdentityExtensions
     {
-        public static bool HasAdminPrivileges(this HttpContext context)
+        public static bool CheckAdmin(this HttpContext context)
         {
             var isAdminClaim = context.User.Claims.SingleOrDefault(x => x.Type == "isAdmin");
             if (isAdminClaim != null && isAdminClaim.Value == "True")
@@ -12,20 +12,19 @@
             return false;
         }
 
-
         public static Guid GetUserId(this HttpContext context)
         {
             var userIdClaim = context.User.Claims.SingleOrDefault(x => x.Type == "userId");
 
             if (userIdClaim == null || !Guid.TryParse(userIdClaim.Value, out var userId))
             {
-                throw new InvalidOperationException("User ID is not valid or not found.");
+                throw new InvalidOperationException("User ID is not valid or not found in the claims.");
             }
 
             return userId;
         }
 
-        public static bool TryGetAuthenticatedUserId(this HttpContext context, out Guid? userId)
+        public static bool IsUserAuthenticated(this HttpContext context, out Guid? userId)
         {
             userId = null;
 
