9c6bcb84793afb02d81d94fc385c276ad37d670b
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-08-03 11:09:01 +0100

    Changing from an api project to just a class library that can generate tokens

---
+++ Movies App/Movies.Identity/TokenMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Movies.Identity
+{
+    public class TokenMiddleware
+    {
+        private readonly IConfiguration _configuration;
+        private readonly RequestDelegate _next;
+
+        public TokenMiddleware(RequestDelegate next, IConfiguration config)
+        {
+            _configuration = config;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            if (token != null)
+            {
+                AttachClaimsToHttpContext(context, token);
+            }
+
+            await _next(context);
+        }
+        private void AttachClaimsToHttpContext(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromDays(365)
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var claims = jwtToken.Claims.ToList();
+
+                var identity = new ClaimsIdentity(claims, "jwt");
+                context.User = new ClaimsPrincipal(identity);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
