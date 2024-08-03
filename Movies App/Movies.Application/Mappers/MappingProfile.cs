46a583c9f9f1ce541f2dd875efc28e545cf8bbe9
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-08-03 14:50:31 +0100

    Appending auto mapper configuration to the mappers folder

---
+++ Movies App/Movies.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Movies.Application.DataTransferObjects;
+using Movies.Application.Models;
+
+namespace Movies.Application.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, MovieDto>()
+                .ForMember(dest => dest.UserWatchlistId, opt => opt.Ignore());
+
+            CreateMap<Cast, CastDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
+
+            CreateMap<Genre, GenreDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<ExternalRating, ExternalRatingDto>();
+
+            CreateMap<OmdbRating, OmdbRatingDto>();
+
+            CreateMap<MovieRating, MovieRatingDto>();
+        }
+    }
+}
