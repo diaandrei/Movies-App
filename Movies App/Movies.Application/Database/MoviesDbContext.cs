8010c101e3c4b752bb872a642016ea86997da943
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-08-04 10:46:34 +0100

    Removed the user created in the db context as it's already present to the database and updating gitignore

--- Movies App/Movies.Application/Database/MoviesDbContext.cs
+++ Movies App/Movies.Application/Database/MoviesDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Movies.Application.Models;
@@ -27,53 +26,6 @@ namespace Movies.Application.Database
         public DbSet<ExternalRating> ExternalRatings { get; set; }
         public DbSet<MovieGenres> MovieGenres { get; set; }
         public DbSet<MovieCast> MovieCast { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<IdentityRole>().HasData(new IdentityRole
-            {
-                Name = "Admin",
-                NormalizedName = "Admin",
-                Id = "1",
-                ConcurrencyStamp = "1"
-            });
-
-            builder.Entity<IdentityRole>().HasData(new IdentityRole
-            {
-                Name = "User",
-                NormalizedName = "User",
-                Id = "2",
-                ConcurrencyStamp = "2"
-            });
-            var adminUserId = Guid.NewGuid().ToString();
-
-            var appUser = new ApplicationUser
-            {
-                Id = adminUserId,
-                Email = "aamoviesapp@gmail.com",
-                EmailConfirmed = true,
-                FirstName = "Movies",
-                LastName = "Admin",
-                UserName = "aamoviesapp@gmail.com",
-                IsTrustedMember = true,
-                IsAdmin = true,
-                NormalizedUserName = "AAMOVIESAPP@GMAIL.COM",
-                NormalizedEmail = "AAMOVIESAPP@GMAIL.COM"
-            };
-
-            PasswordHasher<ApplicationUser> ph = new PasswordHasher<ApplicationUser>();
-            appUser.PasswordHash = ph.HashPassword(appUser, "134013Aa!");
-
-            builder.Entity<ApplicationUser>().HasData(appUser);
-
-            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-            {
-                RoleId = "1",
-                UserId = adminUserId
-            });
-        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
